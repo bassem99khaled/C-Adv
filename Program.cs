d0f35701e106ad96c33f2b6f7c5e2553d6799f5b
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-22 19:26:38 +0200

    Q 01

--- Program.cs
+++ Program.cs
@@ -4,7 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            #region 1. Given an array  consists of  numbers with size N and number of queries,
+            //in each query you will be given an integer X, and you should print
+            //how many numbers in array that is greater than  X .
+            //   Ex  :
+            //       Input 3 3                
+            // Size of array , number of querie
+            // s 11 5 3             //Array   
+            //  1                      //Query  1
+            //  5                     //Query   2
+            //  13                  //Query     3
+            //  Output 3  11,5, 3        1 1   1 0
+
+
+
+
+
+
+            //
+            //  string[] input = Console.ReadLine().Split();
+            //  int n = int.Parse(input[0]);
+            //  int q = int.Parse(input[1]);
+            //
+            //  int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+            //
+            //
+            //  for (int i = 0; i < q; i++)
+            //  {
+            //      int x = int.Parse(Console.ReadLine());
+            //      int count = 0;
+            //
+            //      foreach (int num in array)
+            //      {
+            //          if (num > x)
+            //          {
+            //              count++;
+            //          }
+            //      }     
+            //     Console.WriteLine(count);
+            //  }
+            
+        
+
+
+        #endregion
     }
+}
 }
