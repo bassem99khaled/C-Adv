9fb351665251390d74452179ec203349d2eaddf5
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-22 15:54:11 +0200

    Generic Collections - LinkedList

--- Program.cs
+++ Program.cs
@@ -111,96 +111,112 @@ namespace C__Adv
 
             #region Generic Colloections _ List ( Methods )
 
-        //
-        //    List<int> Numbers = new List<int>(5) { 1, 2, 3, 4, 5 };
-        //
-        //    Numbers.Insert(0, 100);
-        //    Numbers.InsertRange(1, [101, 102, 102, 103, 104, 105]);
-        //
-        //    foreach (int i in Numbers)
-        //    {
-        //        Console.WriteLine(Numbers);
-        //    }
-        //
-        //    IReadOnlyList<int> ReadOnlyNumbers = Numbers.AsReadOnly();
-        //
-        //    Numbers.AddRange([4, 6, 7, 8, 9]);
-        //
-        //    foreach (int i in ReadOnlyNumbers)
-        //    {
-        //        Console.WriteLine(Numbers);
-        //    }
-        //
-        //
-        //  //  int index = Numbers.BinarySearch(100); // -101
-        //
-        //   // Console.WriteLine($" INdex = {index}");
-        //
-        //    List<Employee> Employees = new List<Employee>();
-        //
-        //    {
-        //        new Employee(10, "Ahmed", 4000);
-        //        new Employee(20, "Bassem", 5000);
-        //        new Employee(30, "Osama", 6000);
-        //    }
-        //
-        //    Employees.Sort();
-        //
-        // //   int index = Employees.BinarySearch(new Employee(20, "Bassem", 5000));
-        // //   index = Employees.BinarySearch(new Employee() { Name = " Bassem" }, new EmployeeComaprer);
-        // //   index = Employees.BinarySearch(1, 2,new Employee() { Name = " Bassem" }, new EmployeeComaprer);
-        //
-        //    Console.WriteLine(Employees);
-        //    
-        //    
-        //    Numbers.Clear();
-        //
-        //    Numbers.Contains(10);
-        //
-        //    //    Numbers.ConvertAll<long>(SomeFunctions.Tesat);
-        //    //
-        //    //    class Somefunctions
-        //    //{ 
-        //    //
-        //    //}
-        //    int[] Arr = new int[11];
-        //
-        //    Numbers.CopyTo(Arr ,5  );
-        //
-        //    foreach (int number in Arr)
-        //        Console.WriteLine(number);
-        //
-        //    Numbers.EnsureCapacity(10);
-        //
-        //    int evenNumbers = Numbers.FindLast((N) => N % 2 == 0);
-        //
-        //    int index = Numbers.FindLastIndex (N => N % 2 == 0);
-        //
-        //    int[] evenNumber = Numbers.FindAll ( N => N % 2 == 0 ).ToArray();
-        //
-        //    Console.WriteLine($"Even Nubmer = {evenNumbers} Exsited at index : {index}");
-        //
-        //
-        //
-        //    Numbers.Exists(Hamda => Hamda % 2 == 0);
-        //    Console.Write(isExist);
-        //
-        //
-        //    Numbers.TrueForAll((N) => N % 2 == 0);
-        //
-        //    Numbers.ForEach(N =>
-        //    {
-        //        N += 100;
-        //    });
-        //
-        //
-        //    Numbers.IndexOf(5 , 2 , 3);
-        //
-        //    Numbers.RemoveAll(N => N % 2 == 1);
-        //
-        //    Numbers.Reverse();
-        //
+            //
+            //    List<int> Numbers = new List<int>(5) { 1, 2, 3, 4, 5 };
+            //
+            //    Numbers.Insert(0, 100);
+            //    Numbers.InsertRange(1, [101, 102, 102, 103, 104, 105]);
+            //
+            //    foreach (int i in Numbers)
+            //    {
+            //        Console.WriteLine(Numbers);
+            //    }
+            //
+            //    IReadOnlyList<int> ReadOnlyNumbers = Numbers.AsReadOnly();
+            //
+            //    Numbers.AddRange([4, 6, 7, 8, 9]);
+            //
+            //    foreach (int i in ReadOnlyNumbers)
+            //    {
+            //        Console.WriteLine(Numbers);
+            //    }
+            //
+            //
+            //  //  int index = Numbers.BinarySearch(100); // -101
+            //
+            //   // Console.WriteLine($" INdex = {index}");
+            //
+            //    List<Employee> Employees = new List<Employee>();
+            //
+            //    {
+            //        new Employee(10, "Ahmed", 4000);
+            //        new Employee(20, "Bassem", 5000);
+            //        new Employee(30, "Osama", 6000);
+            //    }
+            //
+            //    Employees.Sort();
+            //
+            // //   int index = Employees.BinarySearch(new Employee(20, "Bassem", 5000));
+            // //   index = Employees.BinarySearch(new Employee() { Name = " Bassem" }, new EmployeeComaprer);
+            // //   index = Employees.BinarySearch(1, 2,new Employee() { Name = " Bassem" }, new EmployeeComaprer);
+            //
+            //    Console.WriteLine(Employees);
+            //    
+            //    
+            //    Numbers.Clear();
+            //
+            //    Numbers.Contains(10);
+            //
+            //    //    Numbers.ConvertAll<long>(SomeFunctions.Tesat);
+            //    //
+            //    //    class Somefunctions
+            //    //{ 
+            //    //
+            //    //}
+            //    int[] Arr = new int[11];
+            //
+            //    Numbers.CopyTo(Arr ,5  );
+            //
+            //    foreach (int number in Arr)
+            //        Console.WriteLine(number);
+            //
+            //    Numbers.EnsureCapacity(10);
+            //
+            //    int evenNumbers = Numbers.FindLast((N) => N % 2 == 0);
+            //
+            //    int index = Numbers.FindLastIndex (N => N % 2 == 0);
+            //
+            //    int[] evenNumber = Numbers.FindAll ( N => N % 2 == 0 ).ToArray();
+            //
+            //    Console.WriteLine($"Even Nubmer = {evenNumbers} Exsited at index : {index}");
+            //
+            //
+            //
+            //    Numbers.Exists(Hamda => Hamda % 2 == 0);
+            //    Console.Write(isExist);
+            //
+            //
+            //    Numbers.TrueForAll((N) => N % 2 == 0);
+            //
+            //    Numbers.ForEach(N =>
+            //    {
+            //        N += 100;
+            //    });
+            //
+            //
+            //    Numbers.IndexOf(5 , 2 , 3);
+            //
+            //    Numbers.RemoveAll(N => N % 2 == 1);
+            //
+            //    Numbers.Reverse();
+            //
 
+            #endregion
+
+
+            #region Generic Collections - LinkedList
+
+         //  LinkedList<int> Numbers = new LinkedList<int>();
+         //
+         //
+         //  LinkedList<int> FirstNode = Numbers.AddFirst(1);
+         //  LinkedList<int> FourNode = Numbers.AddLast(3);
+         //
+         //  LinkedListNode<int> SecondNode = Numbers.AddAfter(FirstNode , 2);
+         //
+         //  LinkedList<int> ThirdNode = Numbers.AddBefore(FourthNode , 3);
+         //
+         //  Console.WriteLine(FirstNode.previous.Value); // 2
             #endregion
 
         }
